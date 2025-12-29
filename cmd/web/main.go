80b20c5ea1404976a0086bcaccffcceada0dcd6f
franciscogonz64 <francisco.gonz64@gmail.com>
2025-12-29 14:07:49 -0800

    Initial commit: scaffold Go project

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+const webPort = "80"
+
+func main() {
+
+	// connect to the database
+
+	// create channels
+
+	// create waitgroup
+
+	// set up application config
+
+	// listen for web connections
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module subscription-concurrency-project
+
+go 1.25.1
